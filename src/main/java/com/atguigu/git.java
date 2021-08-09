7b7ac968a2f3f72113886f1267b0a21e6557d991
markchern <13zxchen3@gmail.com>
2021-08-10 07:05:28 +0800

    hot fix

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -8,5 +8,6 @@ public class git {
         System.out.println("hello,git.");
         System.out.println("hello,git.");
         System.out.println("hello,git.");
+        System.out.println("hello,git.");
     }
 }
